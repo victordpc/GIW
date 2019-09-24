562df626308302cbac32db94a3a0fade32e6e8c4
Victor <victordpc@gmail.com>
2019-09-24 14:33:25 +0200

    Practica 1 Ejercicio 3

--- Practica1_3.py
+++ Practica1_3.py
@@ -1,32 +1,34 @@
 import csv
+import re
+
+def buscarPalabras():
+    regex1 = r'(\W|^)('
+    regex2 = r')(\W|$)'
+    resultado = dict()
+
+    texto=open(fichero2,encoding='utf-8').read()
+    texto=texto.upper()
+
+    # Limpiamos el texto de entrada
+    texto = texto.replace("\n"," ")
+    texto = texto.replace(" ","  ")
+
+    with open(fichero1,encoding='utf-8') as origen:
+        reader = csv.reader(origen, delimiter=',')
+
+        for lista in reader:
+            for palabra in lista:
+                palabra= palabra.upper()
+
+                regex = regex1+palabra+regex2
+
+                x = re.findall(regex, texto)
+                resultado[palabra]=len(x)
+    return resultado
 
 fichero1="file1.csv"
 fichero2="file2.txt"
 
-
-texto=open(fichero2,encoding='utf-8').read()
-texto=texto.upper()
-# Limpiamos el texto de entrada
-texto = texto.replace("\n"," ")
-texto = texto.replace(" ","  ")
-
-regex1 = r'(\W|^)('
-regex2 = r')(\W|$)'
-
-with open(fichero1,encoding='utf-8') as origen:
-    reader = csv.reader(origen, delimiter=',')
-
-    for lista in reader:
-        for palabra in lista:
-            palabra= palabra.upper()
-
-            regex = regex1+palabra+regex2
-
-
-
-cadena = "bienvenido aaplicación mimi mi".capitalize() 
-print (cadena.count("mi") )
-print (cadena.find("mi",0) )
- 
-print (cadena.find("mi", 0, 10) )
-print (cadena.find("mi", 14) )
+resultado = buscarPalabras()
+            
+print(resultado)
