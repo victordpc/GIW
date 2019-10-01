821d9ca9877d7d95c9b768f6a86f938dcbef9800
Victor <victordpc@gmail.com>
2019-10-01 15:02:54 +0200

    Practica 2 Ejercicio 2

--- Vic/Practica2_2.py
+++ Vic/Practica2_2.py
@@ -2,6 +2,7 @@ import csv
 import json
 import math
 
+
 def leer_estaciones():
     resultado = dict()
 
@@ -15,23 +16,104 @@ def leer_estaciones():
 
     return resultado
 
+
 def leer_museos():
-    leer = json.loads(open('Museos.json',encoding="utf8").read())
+    leer = json.loads(open('Museos.json', encoding="utf8").read())
     return leer['@graph']
 
+
 def haversine(lat1, lon1, lat2, lon2):
-    rad=math.pi/180
-    dlat=lat2-lat1
-    dlon=lon2-lon1
-    R=6372.795477598
-    a=(math.sin(rad*dlat/2))**2 + math.cos(rad*lat1)*math.cos(rad*lat2)*(math.sin(rad*dlon/2))**2
-    distancia=2*R*math.asin(math.sqrt(a))
+    rad = math.pi/180
+    dlat = lat2-lat1
+    dlon = lon2-lon1
+    R = 6372.795477598
+    a = (math.sin(rad*dlat/2))**2 + math.cos(rad*lat1) * \
+        math.cos(rad*lat2)*(math.sin(rad*dlon/2))**2
+    distancia = 2*R*math.asin(math.sqrt(a))
     return distancia
 
-def presentar_datos(estaciones,museos):
-    return True
+
+def leer_valores():
+    resultado = dict()
+    with open('Salida.csv', encoding='utf8', errors='ignore') as documento:
+        entrada = csv.reader(documento, delimiter=";")
+
+        for texto in entrada:
+            if texto == []:
+                continue
+            if entrada.line_num == 1:
+                cabecera = texto
+                continue
+
+            tipo = ""
+            max = 0.0
+            for i in range(1, len(texto)):
+                if texto[i] != '---' and float(texto[i]) > max:
+                    max = float(texto[i])
+                    tipo = cabecera[i]
+            resultado[texto[0]] = {
+                "Nombre": texto[0], "Valor": max, "Tipo": tipo}
+    return resultado
+
+
+def presentar_datos(estaciones, valores, museos):
+    resultado = dict()
+    resultado["año"] = 2019
+    resultado["fuente"] = "Ayuntamiento de Madrid"
+    museos = list()
+
+    resultado["museos"] = museos
+
+    with open("Salida.json", "w", encoding='utf8') as salida:
+        json.dump(resultado, salida, indent=2)
 
 
 estaciones = leer_estaciones()
-museos=leer_museos()
-presentar_datos(estaciones,museos)
+museos = leer_museos()
+valores = leer_valores()
+presentar_datos(estaciones, valores, museos)
+
+
+# {
+#   "año": 2019,
+#   "fuente": "Ayuntamiento de Madrid",
+#   "museos": [
+#     {
+#       "museo": "Museo xxxx",
+#       "Estación 1": {
+#         "Nombre": "Estación X",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       },
+#       "Estación 2": {
+#         "Nombre": "Estación Y",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       },
+#       "Estación 3": {
+#         "Nombre": "Estación Z",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       }
+#     },
+#     ...
+#     {
+#       "museo": "Museo xxxx",
+#       "Estación 1": {
+#         "Nombre": "Estación X",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       },
+#       "Estación 2": {
+#         "Nombre": "Estación Y",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       },
+#       "Estación 3": {
+#         "Nombre": "Estación Z",
+#         "Valor": XX,
+#         "Tipo":TipoContaminante
+#       }
+#     }
+#   ]
+# }
